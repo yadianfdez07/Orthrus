92736c7c6498b3ed08e40931f6c32aa3a9fe7262
yadianfdez07@dellpc <yadianfdez07@gmail.com>
2020-06-02 18:26:15 -0400

    Inital commit

---
+++ oligoCode.ToDo.Shell/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace oligoCode.ToDo.Shell.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
