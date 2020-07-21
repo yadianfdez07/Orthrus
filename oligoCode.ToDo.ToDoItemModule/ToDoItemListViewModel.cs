a23274a6de8cc4bf0998217b38275f5dad544b0f
yadianfdez07@dellpc <yadianfdez07@gmail.com>
2020-07-20 20:16:12 -0400

    adding datatemplate to display ToDoItems

--- oligoCode.ToDo.ToDoItemModule/ToDoItemListViewModel.cs
+++ oligoCode.ToDo.ToDoItemModule/ToDoItemListViewModel.cs
@@ -1,6 +1,8 @@
-﻿using Prism.Mvvm;
+﻿using oligoCode.ToDo.Model;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,5 +14,22 @@ namespace oligoCode.ToDo.ToDoItemModule
         private string message = "ToDoItem list view";
 
         public string Message { get => message; set => message = value; }
+
+        private ObservableCollection<ToDoItem> toDoItems;
+        public ObservableCollection<ToDoItem> ToDoItems
+        {
+            get { return toDoItems; }
+            set { SetProperty(ref toDoItems, value); }
+        }
+
+        public ToDoItemListViewModel()
+        {
+            ToDoItems = new ObservableCollection<ToDoItem>(new List<ToDoItem>{
+                new ToDoItem{ Id = 1, Description = "ToDoItem 1 Description"},
+                new ToDoItem{ Id = 2, Description = "ToDoItem 2 Description"},
+                new ToDoItem{ Id = 3, Description = "ToDoItem 3 Description"},
+                new ToDoItem{ Id = 4, Description = "ToDoItem 4 Description" }
+                });
+        }
     }
 }
